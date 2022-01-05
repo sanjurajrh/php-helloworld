85ad78f068b73b6f56d15991d7a07c8507960883
sanjurajrh <84304865+sanjurajrh@users.noreply.github.com>
2022-01-05 08:55:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Updated information". "\n";
-print "Changing the build configuration". "\n";
-print "New code has been added". "\n";
 ?>
